ba7d8e383de38821555d785dc45fc31168829690
Mona Lisa <ka46561@zut.edu.pl>
2022-01-19 18:49:51 +0100

    nowy

--- test2.py
+++ test2.py
@@ -2,10 +2,10 @@ def test_file1():
     assert 1 == 1
   
 def test_file2():
-    assert 1 == 1
+    assert 1 == 123
 
 def test_file3():
-    assert 1 == 1
+    assert 1 == 12
     
 def test_file4():
     assert 1 == 1
