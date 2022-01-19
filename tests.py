ee752028d5fb14b59152f1fc15a9e6f5087a2b38
Mona Lisa <ka46561@zut.edu.pl>
2022-01-19 17:13:55 +0100

    jest

---
+++ tests.py
@@ -0,0 +1,11 @@
+import functions 
+def test_sum():
+    assert functions.f1() == 6, "Should be 6"
+
+def test_sum_tuple():
+    assert functions.f2() == 6, "Should be 6"
+
+if __name__ == "__main__":
+    test_sum()
+    test_sum_tuple()
+    print("Everything passed")
