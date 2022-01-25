626db37635e2c9e7fa9ccc13297b5f0dca5cd684
Mona Lisa <ka46561@zut.edu.pl>
2022-01-25 20:38:53 +0100

    nowytest

--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ def test_sum():
     assert functions.f1() == 6, "Should be 6"
 
 def test_sum_tuple():
-    assert functions.f2() == 6, "Should be 6"
+    assert functions.f2() == 6+10, "Should be 16"
 
 if __name__ == "__main__":
     test_sum()
